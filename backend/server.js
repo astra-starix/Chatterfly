ee096ee827a579f0640f3abb95d720115ad8f8d7
astra-starix <sahilsagar0460@gmail.com>
2024-10-04 23:46:09 +0530

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
-const app = express()
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-})
+  console.log("Server is running on port " + PORT);
+});
