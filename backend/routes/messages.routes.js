784cee07cd6f9d462901ff2199eb499de93352db
astra-starix <sahilsagar0460@gmail.com>
2024-10-18 23:26:00 +0530

    get message

--- backend/routes/messages.routes.js
+++ backend/routes/messages.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { isAuthenticate } from "../middleware/verifyToken.js";
-import { sendMessage } from "../controllers/messages.controller.js";
+import { getMessage, sendMessage } from "../controllers/messages.controller.js";
 
 const router = express.Router()
 
+router.get("/:id", isAuthenticate, getMessage)
 router.post("/send/:id", isAuthenticate, sendMessage)
 
 export default router
