53f3468bd7331c1ea459fb2439e53c79fc47c999
astra-starix <sahilsagar0460@gmail.com>
2024-10-29 02:54:30 +0530

    sidebar setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import Sidebar from "../components/sidebar/Sidebar.jsx";
+
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
-export default Home
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
+      <Sidebar />
+    </div>
+  );
+};
+export default Home;
