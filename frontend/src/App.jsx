b3f51d81798de7da66c2a6658c86d6392f51d3ab
astra-starix <sahilsagar0460@gmail.com>
2024-10-28 21:20:01 +0530

    login page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import {Routes, Route}from "react-router-dom"
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/LogIn";
 const App = () => {
-  return <div className="text-red-300">App</div>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+      </Routes>
+    </div>
+  );
 };
 export default App;
