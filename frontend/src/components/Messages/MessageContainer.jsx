e63d0b638b9d3a22266565925b39a8a79eac3e12
astra-starix <sahilsagar0460@gmail.com>
2024-10-29 22:49:47 +0530

    errors

--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -3,7 +3,7 @@ import Messages from "./Messages";
 
 const MessageContainer = () => {
   return (
-    <div className="md:min-w-[450] flex flex-col">
+    <div className="md:min-w-[450px] flex flex-col">
       <>
         <div className="bg-slate-500 px-4 py-2 mb-2">
           <span className="label-text">To: </span>
