3fe802c11869088b6df99f6792fe73b1fa64282d
astra-starix <sahilsagar0460@gmail.com>
2024-10-29 14:48:40 +0530

    Message Container

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import MessageContainer from "../components/Messages/MessageContainer.jsx";
 import Sidebar from "../components/sidebar/Sidebar.jsx";
 
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
       <Sidebar />
+
+      <MessageContainer/>
     </div>
   );
 };
